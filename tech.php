15459cc0ffc63f78db2779430eedeae5a470dd42
Harim Jeong <harim97714@mail.hongik.ac.kr>
2021-01-14 16:11:45 +0900

    update

--- tech.php
+++ tech.php
@@ -12,7 +12,13 @@
     </header>
     <nav>
         <ul id="menu">
+            <li><a href="about_me.php">자기소개</a></li>
             <li><a href="tech.php">기술정리</a></li>
+            <li><a href="timetable.php">시간표</a></li>
+            <li><a href="favorite_song.php">좋아하는 노래</a></li>
+            <li><a href="stature.php">예상 키 계산하기</a></li>
+            <li><a href="#">미정 메뉴 1</a></li>
+            <li><a href="#">미정 메뉴 2</a></li>
         </ul>
     </nav>
     <section>
