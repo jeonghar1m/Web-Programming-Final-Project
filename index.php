de24a018f04d8543e0e53d8903c213ebf7e9f882
Harim Jeong <harim97714@mail.hongik.ac.kr>
2021-01-16 21:28:07 +0900

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <h1>Main Page</h1>
         <hr>
         <p>2020년 겨울학기 웹프로그래밍 기말과제로 제출한 포트폴리오입니다.</p>
-		<p>테스트 환경: <a href="http://www1.jeongharim.com">개인 웹호스팅 공간</a></p>
+		<p>테스트 환경: <a style="color:blue;" href="http://www1.jeongharim.com">개인 웹호스팅 공간</a></p>
         <h2>사이트맵</h2>
         <ul>
             <li>메인페이지</li>
