be0e2be89cdc9c4b766763b97eefbc8fe0e947be
Harim Jeong <harim97714@mail.hongik.ac.kr>
2021-01-19 01:21:51 +0900

    title update

--- now_time.php
+++ now_time.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>정하림</title>
+    <title>정하림 - 현재 시간</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script>
         window.onload = function() {
--- result.php
+++ result.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>정하림</title>
+    <title>정하림 - 예상 키 계산하기</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
